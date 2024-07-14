e671962a3e0e88307933ae1628fdfe9235d6653f
Miguel <63953943+sousa-miguel@users.noreply.github.com>
2024-07-14 19:58:41 +0100

    feat: Add concept of User (#2)
    *Add concept of Users
    *Create utils package
    *Prepare code for authenticated requests
    *Update README.md

---
+++ routes/users.go
@@ -0,0 +1,52 @@
+package routes
+
+import (
+	"net/http"
+
+	"example.com/rest-api-gin/models"
+	"example.com/rest-api-gin/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse data"})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not create user"})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "user created successfully!"})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse data"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+		return
+	}
+
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not authenticate user"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "login successfull!", "token": token})
+}
