2b158f2785c0d205dc1360c1101ff10ac0ab99cb
Miguel Sousa <miguel.am.sousa@ctw.bmwgroup.com>
2024-07-14 13:44:07 +0100

    init

---
+++ db/db.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	DB, err := sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Database failed to start...")
+	}
+
+	DB.SetMaxOpenConns(5)
+	DB.SetMaxIdleConns(3)
+
+	createTables()
+}
+
+func createTables() {
+	createEventsTable := `
+CREATE TABLE IF NOT EXISTS events (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	description TEXT NOT NULL,
+	location TEXT NOT NULL,
+	dateTime DATETIME NOT NULL,
+	user_id INTEGER
+)
+`
+	_, err := DB.Exec(createEventsTable)
+
+	if err != nil {
+		panic("Could not create events table")
+	}
+}
